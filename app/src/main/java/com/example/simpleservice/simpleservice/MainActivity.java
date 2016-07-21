2b1c593feaef77091bcd59d76b25c1b14fdcb84c
rodoggx <mikerodolph@yahoo.com>
2016-07-21 15:05:36 -0400

    My First Commit

--- app/src/main/java/com/example/simpleservice/simpleservice/MainActivity.java
+++ app/src/main/java/com/example/simpleservice/simpleservice/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.simpleservice.simpleservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("We are in the onCreate of the activity");
+
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
     }
+
 }
---
+++ app/src/main/java/com/example/simpleservice/simpleservice/MyService.java
@@ -0,0 +1,23 @@
+package com.example.simpleservice.simpleservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        System.out.println("We are in the onCreate of the Service");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
